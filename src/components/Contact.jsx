d4ee3f60744454b1655758327baa2d9da845aa1d
JimJam117 <jamessparrow101@gmail.com>
2020-03-10 18:43:39 +0000

    Added state to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Link} from 'react-router-dom';
 
@@ -6,6 +6,12 @@ import Header from './partials/Header';
 import Footer from './partials/Footer';
 
 const Contact = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [body, setBody] = useState("");
+
+
     return (
         <div className="main-container">
             <Header />
@@ -13,9 +19,30 @@ const Contact = () => {
         <main>
             <div className="banner"><h1>Contact</h1></div>
 
-            <main>
-                Contact form
-            </main>
+            <form action="/contact" method="post" enctype="multipart/form-data">
+
+    <div className=" form-group row">
+        <input id="name" placeholder="Name" type="name" className="form-control @error('name') is-invalid @enderror"
+            name="name" onChange={(e) => setName(e.target.value)} value={name} required />
+    </div>
+
+    <div className=" form-group row">
+        <input id="email" type="email" placeholder="Email" className="form-control @error('email') is-invalid @enderror"
+            name="email" onChange={(e) => setEmail(e.target.value)} value={email} required />
+    </div>
+
+
+    <div className=" form-group row">
+        <textarea placeholder="Message" className="form-control @error('body') is-invalid @enderror" type="text"
+            name="body" rows="8" onChange={(e) => setBody(e.target.value)}>{body}</textarea>
+    </div>
+
+    <div className="form-group row mb-0">
+        <button aria-label="Send" className="submit-button" type="submit" name="button">Send</button>
+    </div>
+</form>
+
+{/* {!! NoCaptcha::renderJs() !!} */}
 
             <Footer />
         </main>
